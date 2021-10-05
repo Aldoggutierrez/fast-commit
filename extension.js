5439118ceb29e70724adbff7a55dca9e1148192b
aldogtz <aldogtz1998@gmail.com>
2021-10-05 00:21:34 -0500

    commit

--- extension.js
+++ extension.js
@@ -17,14 +17,17 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let script = vscode.commands.registerCommand('identificador.runscript', function () {
+	let disposable = vscode.commands.registerCommand('identificador.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from prueba!');
+		vscode.commands.executeCommand('git.stageAll');
+		vscode.commands.executeCommand('git.commitStaged');
+		vscode.commands.executeCommand('git.pull');
+		vscode.commands.executeCommand('git.push');
 	});
 
-	context.subscriptions.push(script);
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
